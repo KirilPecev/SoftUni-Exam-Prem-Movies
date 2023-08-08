1f752d5347cc43d263a9bc334958752eb886a2c2
KirilPecev <kirilpecev@gmail.com>
2023-08-08 23:37:49 +0300

    ui test fixes

--- tests/ui/movies.test.js
+++ tests/ui/movies.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check movies page', async ({ page }) => {
-    await page.goto('http://softuni-exam-prep-movies.onrender.com');
+    await page.goto('http://softuni-exam-prep-movies.onrender.com/movies');
     const list = await page.$('ul');
     expect(list).toBeTruthy();
   });
